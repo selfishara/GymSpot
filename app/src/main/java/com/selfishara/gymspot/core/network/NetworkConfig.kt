26051dd781df45be2839369fe1d45dc8bbc73337
Selfishara <saramb10.smb@gmail.com>
2026-03-20 17:24:54 +0100

    feat: setup Ktor http client with JSON serialitzation

---
+++ app/src/main/java/com/selfishara/gymspot/core/network/NetworkConfig.kt
@@ -0,0 +1,12 @@
+package com.selfishara.gymspot.core.network
+
+/**
+ * NetworkConfig holds the configuration constants for network operations.
+ *
+ * This includes the base URL for API requests and any other related settings
+ * that may be needed for network communication.
+ */
+
+object NetworkConfig {
+    const val BASE_URL = "https://api.gymspot.com/"
+}
